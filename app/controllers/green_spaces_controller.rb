e1dc5343a9f0281124b1338ffc6d1b78641860aa
Michael Wellman <michaelrwellman@gmail.com>
2019-04-23 13:52:30 -0400

    added new green space form

--- app/controllers/green_spaces_controller.rb
+++ app/controllers/green_spaces_controller.rb
@@ -9,9 +9,29 @@ class GreenSpacesController < ApplicationController
 
   end
 
+  def new
+    @green_space = GreenSpace.new
+  end
+
+  def create
+    @green_space = GreenSpace.new(new_green_space_params)
+    if @green_space.save
+      flash[:notice] = "Green Space added successfully"
+      redirect_to green_space_path(@green_space)
+    else
+      flash.now[:alert] = @green_space.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
   private
 
   def set_green_space
     @green_space = GreenSpace.find(params[:id])
   end
+
+  def new_green_space_params
+    require(:green_space).permit(:name, :description)
+  end
+
 end
