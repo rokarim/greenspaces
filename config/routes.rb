682f6771e48214c7b4fc04dbe7be298434699108
rokarim <romina.k@gmail.com>
2019-04-23 14:43:33 -0400

    pat fixed his stupid error

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  root 'homes#index'
-  
-=======
   root 'green_spaces#index'
->>>>>>> 49e8fff831e1600f3a29b74f09de3fe5add164e0
   devise_for :users
 
   resources :green_spaces, only: [:index, :show]
