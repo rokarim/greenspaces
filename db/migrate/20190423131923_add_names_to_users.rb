9b36c15c84dfedca0a92eee01a032e7f67fa08bc
rokarim <romina.k@gmail.com>
2019-04-23 10:36:40 -0400

    Devise configuration completed.
    First and last name added to User model and migrations created for that.
    
    Capybara test updated and Unit test created for User.

---
+++ db/migrate/20190423131923_add_names_to_users.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class AddNamesToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :last_name, :string, null: false
+  end
+end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,6 +3,8 @@ require 'factory_bot'
 FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
+    first_name { 'Betty' }
+    last_name { 'Boop' }
     password { 'password' }
     password_confirmation { 'password' }
   end
