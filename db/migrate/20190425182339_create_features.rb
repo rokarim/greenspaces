9360dba3e9c3f8e1cd64b47b8d3550e635f69b0a
Drew Baker <drew.baker914@gmail.com>
2019-04-29 16:32:50 -0400

    feature uniqueness validations

--- db/migrate/20190425182339_create_features.rb
+++ db/migrate/20190425182339_create_features.rb
@@ -1,7 +1,7 @@
 class CreateFeatures < ActiveRecord::Migration[5.2]
   def change
     create_table :features do |t|
-      t.string :name, null: false
+      t.string :name, null: false, unique: true
 
       t.timestamps
     end
