3ff74fe99ad1dab8e3b7b84d736e3fef36f5229c
Drew Baker <drew.baker914@gmail.com>
2019-04-29 15:59:57 -0400

    uniqueness validation for neighborhoods

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,5 +1,5 @@
 class Neighborhood < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   has_many :green_spaces
 end
--- db/migrate/20190426171735_create_neighborhoods.rb
+++ db/migrate/20190426171735_create_neighborhoods.rb
@@ -1,7 +1,7 @@
 class CreateNeighborhoods < ActiveRecord::Migration[5.2]
   def change
     create_table :neighborhoods do |t|
-      t.string :name, null: false
+      t.string :name, null: false, unique: true
 
       t.timestamps
     end
