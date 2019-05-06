e65d911a7b6ac05972c5cb2cb4771b34f7242700
Michael Wellman <michaelrwellman@gmail.com>
2019-05-06 00:30:57 -0400

    Made user reviews dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
-  has_many :reviews
+  has_many :reviews, :dependent => :delete_all
   has_many :green_spaces, through: :reviews
   has_many :votes
 
